60383fa155aa56b3383cbf551cb6982546e8f17d
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-12-31 21:20:53 +1100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 This Country Does Not Exist by jack-debug
 </title>
 <body>
+<?PHP
+echo shell_exec("python countryselec.py 'parameter1'");
+?>
 <h1><u>This Country Does not Exist<u/></h1>
 <p>This Country Does Not Exist is an AI experiment involving Generative Adversial Neural Networks and Recurrent Neural Networks</p>
 </body>
